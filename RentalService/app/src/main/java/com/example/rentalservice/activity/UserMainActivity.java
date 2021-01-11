b558bb08664691126e8d0aab5bfbcd6ad7706b1a
JeongJiYeong <jjy0709@kaist.ac.kr>
2021-01-12 03:01:52 +0900

    TAB LAYOUT 변경

--- RentalService/app/src/main/java/com/example/rentalservice/activity/UserMainActivity.java
+++ RentalService/app/src/main/java/com/example/rentalservice/activity/UserMainActivity.java
@@ -29,8 +29,7 @@ public class UserMainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.user_tool_bar);
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
-        actionBar.setTitle("RentalService");
-
+        actionBar.setTitle("공공장비 온라인센터");
     }
 
 }
