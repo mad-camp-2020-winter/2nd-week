d0e36cc04844a1245ef207a5aaad980dcef9b032
zeroStone <lysuk96@hanyang.ac.kr>
2021-01-12 23:24:58 +0900

    무야호 애니메이션 추가

--- RentalService/app/src/main/java/com/example/rentalservice/activity/SplashActivity.java
+++ RentalService/app/src/main/java/com/example/rentalservice/activity/SplashActivity.java
@@ -5,6 +5,8 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,11 +20,15 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        Animation mAnim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate); // 선글라스
+        mAnim.setInterpolator(getApplicationContext(),android.R.anim.accelerate_interpolator);
+
         ImageView imageView = findViewById(R.id.splash_gif);
         Glide.with(this).load(R.raw.splash_gif).into(imageView);
         MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.splash_music);
         mediaPlayer.start();
 
+        imageView.startAnimation(mAnim);
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
@@ -31,7 +37,7 @@ public class SplashActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        }, 2000);
+        }, 3000);
     }
 
     @Override
